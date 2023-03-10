29b9c7f9ba8151eba2a6784b23627f1c0bbb201f
namdh1111 <106732883+namdh1111@users.noreply.github.com>
2023-03-10 19:41:15 +0700

    update times

--- namdo/As3/action.js
+++ namdo/As3/action.js
@@ -1,7 +1,8 @@
 const result = document.getElementById("result");
 const calculating = document.getElementById("calculating");
+const times = document.getElementById("times");
 let resultReset = '';
-
+timeHistory();
 function append(value) {
     if (resultReset == true || calculating.innerHTML == 0){
         calculating.innerHTML = '';
@@ -17,10 +18,23 @@ function backSpace() {
 function calculate() {
     result.innerHTML = eval(calculating.innerHTML);
     resultReset = true;
+    if (sessionStorage.getItem('times')){
+        sessionStorage.setItem('times',Number(sessionStorage.getItem('times')) + 1);
+    }else {
+        sessionStorage.setItem('times',1);
+    }
+    times.innerHTML = sessionStorage.getItem('times');
 
 }
 function clearResult() {
     calculating.innerHTML = 0;
     result.innerHTML = 0;
     resultReset = true;
+}
+function timeHistory(){
+    if (sessionStorage.getItem('times') == null){
+        times.innerHTML = 0;
+    }else {
+        times.innerHTML = sessionStorage.getItem('times');
+    }
 }
